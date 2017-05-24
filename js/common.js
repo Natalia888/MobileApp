4217a07e08c67d4d026dec1ec3f3405c8596094d
Natalia <vakulenkonatalia8@gmail.com>
2017-05-24 15:15:22 +0200

    Update common.js

--- js/common.js
+++ js/common.js
@@ -76,6 +76,20 @@ $(document).ready(function() {
 		return false;
 	});
 	
-	
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/      
+	$("#callback").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("#callback").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
 
 });
